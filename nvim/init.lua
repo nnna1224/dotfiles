fb2890eba248b574bfa990a406184f78c9deb963
nnna1224 <nagidec.24a@gmail.com>
2026-01-29 23:11:30 +0900

    modify clipboard mode

--- nvim/init.lua
+++ nvim/init.lua
@@ -35,6 +35,18 @@ vim.opt.confirm = true
 vim.opt.splitright = true
 vim.opt.signcolumn = "yes"
 
+vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+        ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+    },
+    paste = {
+        ['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+        ['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+    },
+}
+
 require('catppuccin').setup({
     flavour = "mocha",
     transparent_background = true,
